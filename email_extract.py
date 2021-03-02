dc826a60fc7ea12901bfdc6df6898de49bfccc94
JolonB <jolon.behrent@gmail.com>
2021-03-02 21:59:43 +1300

    Finish script

--- email_extract.py
+++ email_extract.py
@@ -13,52 +13,22 @@ Code heavily inspired by:
 https://www.tutorialspoint.com/python_network_programming/python_imap.htm
 """
 
-regex = re.compile("((?:(https?|s?ftp):\/\/)?(?:www\.)?((?:(?:[A-Z0-9][A-Z0-9-]{0,61}[A-Z0-9]\.)+)([A-Z]{2,6})|(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))(?::(\d{1,5}))?(?:(\/\S+)*))", re.IGNORECASE)
+url_regex = re.compile("((?:(https?|s?ftp):\/\/)?(?:www\.)?((?:(?:[A-Z0-9][A-Z0-9-]{0,61}[A-Z0-9]\.)+)([A-Z]{2,6})|(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))(?::(\d{1,5}))?(?:(\/\S+)*))", re.IGNORECASE)
+url_char_regex = re.compile("[A-Za-z0-9\-\._~:/\?#\[\]@!\$&'\(\)\*\+,;%=]*")
+filename_regex = re.compile("name=\"([A-Za-z0-9_\-,()\.]*)\"")
 
-def parse_msg(parsedbytes) -> dict:
-    msg_full = (
-        parsedbytes.get_payload()[0].get_payload()
-        if parsedbytes.is_multipart()
-        else parsedbytes.get_payload()
-    )
-    print("1")
-    print(msg_full)
-    print("2")
-    # msg_full = msg_full.split("\r\n", 1)
-    return {
-        "from": parsedbytes.get("From"),
-        "to": parsedbytes.get("To"),
-        "subject": parsedbytes.get("Subject"),
-        "header": msg_full[0],
-        "body": msg_full[1],
-    }
+def decode_save_image(msg_part):
+    # get payload
+    payload = msg_part.get_payload()
+    # convert payload from b64 to normal
+    payload = base64.b64decode(payload)
+    # save as "name"
+    filename = filename_regex.match(msg_part.get("Content-Type").split("; ")[1])[1]
+    print("Saving: {}".format(filename))
 
-def decode_image():
-    pass
-
-def save_img(a,b):
-    pass
-
-def check_base(bytestr)->int:
-    # convert to bytes
-    if isinstance(bytestr, str):
-        bytestr = bytestr.encode()
-    if not isinstance(bytestr, bytes):
-        raise ValueError("Trying to check base of a non-string or -bytes object")
-    #
-    # remove \n
-    bytestr = bytestr.replace(b'\n', b'')
-    print(bytestr)
-    #
-    try:
-        if base64.b64encode(base64.b64decode(bytestr)) == bytestr:
-            return 64
-        elif base64.b32encode(base64.b32decode(bytestr)) == bytestr:
-            return 32
-        else:
-            return 0
-    except binascii.Error:
-        return 0
+    # write file
+    with open(filename,'wb') as f:
+        f.write(payload)
 
 def replace(string, replacements):
     rep = dict((re.escape(k), v) for k, v in replacements.items())
@@ -66,7 +36,7 @@ def replace(string, replacements):
     return pattern.sub(lambda m: replacements[re.escape(m.group())], string)
 
 def get_urls(string) -> list:
-    return [t[0] for t in regex.findall(string)]
+    return [url_char_regex.match(t[0])[0] for t in url_regex.findall(string)]
 
 def mkchdir(dir_):
     # create dir
@@ -95,8 +65,6 @@ if not email_id:
 else:
     print_info("You've got mail!")
 
-print(email_id)
-
 parser = Parser()
 
 # create image dir
@@ -110,19 +78,27 @@ for d in email_id:
     from_ = parsedbytes.get("From")
     mkchdir(replace(from_,{"\\<":"","\\>":"","\\\"":""}))
 
+    # save images and extract urls
     for part in parsedbytes.walk():
-        if part.get_content_type().startswith("image"):
-            img = decode_image() # TODO image saving
-            save_img(img, "filename")
-        if part.get_content_type() == "text/plain":
+        content_type = part.get("Content-Type")
+        # save image
+        if content_type.startswith("image"):
+            decode_save_image(part)
+        # save links
+        elif content_type.startswith("text/plain"):
             # convert to regular text if encoded
             if part.get('Content-Transfer-Encoding') == "base64":
                 part = base64.b64decode(str(part.get_payload()))
             # get URLs
             urls = get_urls(str(part))
-            with open("urls.txt", 'a') as url_file:
-                for url in urls:
-                    url_file.write("{}\n".format(url))
+            if urls:
+                with open("urls.txt", 'a') as url_file:
+                    for url in urls:
+                        url_file.write("{}\n".format(url))
+        # keep a record of any other content in the email
+        elif not content_type.startswith(("multipart/","text/html")):
+            with open("other.txt", 'a') as other_file:
+                other_file.write("\n{}".format(content_type))
 
     os.chdir("../")
 
