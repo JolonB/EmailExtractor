d9c5af09ddc49f838e7decdfc70b5420796c1c53
JolonB <jolon.behrent@gmail.com>
2021-03-22 23:25:19 +1300

    Improve duplicate finding speed

--- remove_dup.sh
+++ remove_dup.sh
@@ -1,24 +1,25 @@
-TMP_DIR=./comb_out_tmp/
+ORIG_DIR=./out_flat/
+TMP_DIR=./out_tmp/
 
-rm -rf $TMP_DIR
-mkdir $TMP_DIR
-cp -r ./comb_out/* $TMP_DIR
+rm -rf "$TMP_DIR"
+mkdir "$TMP_DIR"
 
-cd $TMP_DIR
-echo "Begin resize"
-mogrify -resize 100x100 -colors 32 -depth 4 *
-echo "Finish resize"
-echo "Begin compare"
-for img in *; do
-    for img2 in *; do
-        if [[ $img != $img2 ]]; then
-            ph=$(compare -metric phash "$img" "$img2" null: 2>&1)
-            if  [[ $(echo "$ph < 2" | bc) -eq 1 ]]; then
+for name in $ORIG_DIR*; do
+    img=${name##*/}
+    cp "$ORIG_DIR/$img" "$TMP_DIR/$img"
+    mogrify -resize 100x100 -colors 32 -depth 4 "$TMP_DIR/$img"
+    for name2 in $TMP_DIR/*; do
+        img2=${name2##*/}
+        if [[ "$img" != "$img2" ]]; then
+            ph=$(compare -metric phash "$TMP_DIR/$img" "$TMP_DIR/$img2" null: 2>&1)
+            if  [[ $(echo "$ph < 5" | bc) -eq 1 ]]; then
                 echo "$img and $img2 are similar"
             fi
         fi
     done
 done
-echo "Finish compare"
+echo "Done"
+
+rm -rf "$TMP_DIR"
 
 # fdupes -frd .
